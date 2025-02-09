de7314b2f768a734c9b9ac6b55aa35a33c76b5db
Joy Rutledge <joyrutl03@gmail.com>
2025-02-09 01:38:34 -0500

    updated api

--- guestbook-ui/src/componets/Register/Register.jsx
+++ guestbook-ui/src/componets/Register/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:3000/register", {
+    const response = await fetch("https://guestbook-su59.onrender.com/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ fullName, email }),
