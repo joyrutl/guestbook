f2fc5ed8ebe019fd057f81e4b552b03b7af2a94b
Joy Rutledge <joyrutl03@gmail.com>
2025-02-15 23:50:38 -0500

    remove register route

--- guestbook-ui/src/componets/App/App.jsx
+++ guestbook-ui/src/componets/App/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-    <Route path="/register" element={<Register />} />
+    <Route path="/" element={<Register />} />
       </Routes>
       </BrowserRouter>
       
